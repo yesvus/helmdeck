eb4e3884708fcdbab390a5a8341b78432906b006
yesvus <yesvus@gmail.com>
2026-09-24 04:20:05 +0300

    Split media workflows from the core package

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 export * from "./adapters/index.js";
 export * from "./i18n.js";
-export * from "./media/index.js";
 export * from "./primitives/index.js";
 export * from "./shell/index.js";
 export * from "./theme/index.js";
